0f30a417349063fc8243e70208bc50d4552b983d
alanMarcosA <alanaspi1997@gmail.com>
2020-05-21 15:36:30 -0300

    actualizando variablles

--- javas/js.js
+++ javas/js.js
@@ -1,57 +1,74 @@
-function nuevo_juego(){
-    var celda=document.querySelectorAll("p")
-    for (let i = 0; i < celda.length; i++) {
-        celda[i].innerText="";
-        }
+function nuevo_juego() {
+  let celda = document.querySelectorAll("p");
+  for (let i = 0; i < celda.length; i++) {
+    celda[i].innerText = "";
+  }
 }
-function jugar(pos){
-    var celda=document.querySelectorAll("p");
-    if(celda[pos].innerText!=""){
-        alert("poscicion invalida");
-    }else{
-        if(turno==="X"){
-            celda[pos].innerText="X"
-            analizarVictoria(turno)
-            turno="O"
-        }else{
-            celda[pos].innerText="O"
-            analizarVictoria(turno)
-            turno="X"
-        }
-
+function jugar(pos) {
+  let celda = document.querySelectorAll("p");
+  if (celda[pos].innerText != "") {
+    alert("poscicion invalida");
+  } else {
+    if (turno === "X") {
+      celda[pos].innerText = "X";
+      analizarVictoria(turno);
+      turno = "O";
+    } else {
+      celda[pos].innerText = "O";
+      analizarVictoria(turno);
+      turno = "X";
     }
+  }
 }
-function analizarVictoria(turn){
-    var tablero=document.querySelectorAll("p")
-    var gano=false;
-    if((tablero[0].innerText===tablero[1].innerText&& tablero[1].innerText===tablero[2].innerText && tablero[2].innerText===turn) ||
-        (tablero[3].innerText===tablero[4].innerText&& tablero[4].innerText===tablero[5].innerText && tablero[5].innerText===turn) ||
-        (tablero[6].innerText===tablero[7].innerText&& tablero[7].innerText===tablero[8].innerText && tablero[8].innerText===turn) ||
-        (tablero[0].innerText===tablero[3].innerText&& tablero[3].innerText===tablero[6].innerText && tablero[6].innerText===turn) ||
-        (tablero[1].innerText===tablero[4].innerText&& tablero[4].innerText===tablero[7].innerText && tablero[7].innerText===turn) ||
-        (tablero[2].innerText===tablero[5].innerText&& tablero[5].innerText===tablero[8].innerText && tablero[8].innerText===turn) ||
-        (tablero[0].innerText===tablero[4].innerText&& tablero[4].innerText===tablero[8].innerText && tablero[8].innerText===turn) ||
-        (tablero[2].innerText===tablero[4].innerText&& tablero[4].innerText===tablero[6].innerText && tablero[6].innerText===turn))
-    {
-        gano=true
-    }
-    if(gano){
-        alert("gano el jugador " + turn)
-        nuevo_juego()
-    }else if(tableroLleno(tablero)){
-        alert("empate")
-        nuevo_juego()
-    }
+function analizarVictoria(turn) {
+  let tablero = document.querySelectorAll("p");
+  let gano = false;
+  if (
+    (tablero[0].innerText === tablero[1].innerText &&
+      tablero[1].innerText === tablero[2].innerText &&
+      tablero[2].innerText === turn) ||
+    (tablero[3].innerText === tablero[4].innerText &&
+      tablero[4].innerText === tablero[5].innerText &&
+      tablero[5].innerText === turn) ||
+    (tablero[6].innerText === tablero[7].innerText &&
+      tablero[7].innerText === tablero[8].innerText &&
+      tablero[8].innerText === turn) ||
+    (tablero[0].innerText === tablero[3].innerText &&
+      tablero[3].innerText === tablero[6].innerText &&
+      tablero[6].innerText === turn) ||
+    (tablero[1].innerText === tablero[4].innerText &&
+      tablero[4].innerText === tablero[7].innerText &&
+      tablero[7].innerText === turn) ||
+    (tablero[2].innerText === tablero[5].innerText &&
+      tablero[5].innerText === tablero[8].innerText &&
+      tablero[8].innerText === turn) ||
+    (tablero[0].innerText === tablero[4].innerText &&
+      tablero[4].innerText === tablero[8].innerText &&
+      tablero[8].innerText === turn) ||
+    (tablero[2].innerText === tablero[4].innerText &&
+      tablero[4].innerText === tablero[6].innerText &&
+      tablero[6].innerText === turn)
+  ) {
+    gano = true;
+  }
+  if (gano) {
+    alert(`gano el jugador ${turn}`);
+    nuevo_juego();
+  } else if (tablero.find(tableroLleno2(tablero))) {
+    alert("empate");
+    nuevo_juego();
+  }
 }
-function tableroLleno(tablero){
-    var lleno= true
-    for (let i = 0; i < tablero.length; i++) {
-        if(tablero[i].innerText===""){
-            lleno=false
-        }
-        
+function tableroLleno(tablero) {
+  let lleno = true;
+  for (let i = 0; i < tablero.length; i++) {
+    if (tablero[i].innerText === "") {
+      lleno = false;
     }
-    return lleno;
+  }
+  return lleno;
 }
-turno="x"
-var contadorJugadas=0;
+function tableroLleno2(value) {
+  return value.innerText === "";
+}
+turno = "x";
